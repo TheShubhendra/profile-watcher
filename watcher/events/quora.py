252267aae0ad9be23f4f02235e57fe238ba2bbed
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-04-11 17:31:01 +0530

    Add files

---
+++ watcher/events/quora.py
@@ -0,0 +1,10 @@
+
+class FollowingCountIncrease:
+    def __init__(self,
+    previousCount,
+    newCount,
+    countChange
+    ):
+        self.previousCount = previousCount
+        self.newCount = newCount
+        self.countChange = countChange
